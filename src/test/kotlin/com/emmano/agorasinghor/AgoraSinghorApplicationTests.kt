a0ce0df5ddd8d0e169ef659d08c4e0183e6e8af7
Emmaunuel Edorh <emmanoedorh@gmail.com>
2021-08-04 02:52:22 -0500

    Initial commit

---
+++ src/test/kotlin/com/emmano/agorasinghor/AgoraSinghorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.emmano.agorasinghor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgoraSinghorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
